9b76413df01ca2b6383c090c87fcf7c80be8b836
Tia Esguerra <msksfo@gmail.com>
2018-08-03 07:53:55 -0700

    Make Book a functional component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css'
 import PropTypes from 'prop-types'
 
-class Book extends Component {
-    constructor(props){
-        super(props)
-    }
+function Book(props) {
+    
+    return (
+        <div id={props.id} className="book">
+            <div className="book-top">
 
-    render() {
+                {/* if the json data returns an image thumbnail for the book, display it as the background image for the book component. otherwise set a backgrouond color. */}
+                
+                { (props.bookCover && props.bookCover.thumbnail)
+                    ? <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.bookCover.thumbnail})`}}></div>
 
-        return (
-            <div id={this.props.id} className="book">
-                <div className="book-top">
+                    : <div className='book-cover' style={{ width: 128, height: 193, background: '#becace'}}></div>
+                }
+                
+                <div className="book-shelf-changer">
+                    <select  defaultValue={props.selected} onChange={props.onChange}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+                
+            </div> 
 
-                    {/* if the json data returns an image thumbnail for the book, display it as the background image for the book component. otherwise set a backgrouond color. */}
-                    
-                    { (this.props.bookCover && this.props.bookCover.thumbnail)
-                        ? <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.bookCover.thumbnail})`}}></div>
-
-                        : <div className='book-cover' style={{ width: 128, height: 193, background: '#becace'}}></div>
-                    }
-                    
-                    <div className="book-shelf-changer">
-                        <select  defaultValue={this.props.selected} onChange={this.props.onChange}>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
-                    </div>
-                    
-                </div> 
-
-                <div className="book-title">{this.props.bookTitle}</div>
-                <div className="book-authors">{this.props.authors}</div>
-            </div>
-        )
-    }
-
+            <div className="book-title">{props.bookTitle}</div>
+            <div className="book-authors">{props.authors}</div>
+        </div>
+    )
 }
 
 
