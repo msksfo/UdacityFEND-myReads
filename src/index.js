7690f12425a5ec5bf1029d0b3d5b5a4d0035f34d
Tia Esguerra <msksfo@gmail.com>
2018-08-04 14:09:05 -0700

    Fix stupid typo in basename string

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.render(
-    <BrowserRouter basename={"/UdacityFEND-myReads"}> 
+    <BrowserRouter basename="/UdacityFEND-myReads"> 
         <App />
     </BrowserRouter>, document.getElementById('root')
 )
