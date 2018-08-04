6c92c6daf3e35d82acccbae90cc69a600c20b76b
Tia Esguerra <msksfo@gmail.com>
2018-08-03 18:13:06 -0700

    Add header quote to App state. Get a new quote when user returns from search page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import Quote from './Quote';
 import '../App.css';
 
-function Header() {
+function Header(props) {
     
     return (
         <div className="list-books-title">
             <div className="content-wrapper">
-                <div className="quote-container">
-                    <q className="quote">Every story I create, creates me. I write to create myself</q>
-                    <p className="quote-author">~ Octavia E. Butler</p>
-                </div>
-                
+                <Quote quote={props.quote}/>   
                 <h1>MyReads</h1>
             </div>
         </div>
